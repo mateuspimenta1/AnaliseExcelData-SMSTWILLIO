6e76e014e74386aed076b6e8dbd030c5919b24bd
mateuspimenta1 <127818313+mateuspimenta1@users.noreply.github.com>
2023-03-14 03:25:43 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import twilio
 import os
 from twilio.rest import Client
 
-account_sid = "Your User ID"
+account_sid = "Your User SID"
 auth_token = "Your Account Token"
 client = Client(account_sid, auth_token)
 
@@ -13,12 +13,12 @@ listaMeses = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho']
 for mes in listaMeses:
     tabelaVendas = pd.read_excel(f'{mes}.xlsx')
     if (tabelaVendas['Vendas'] > 50000).any():
-        vendedor = tabelaVendas.loc[tabelaVendas['Vendas'] > 50000, 'Vendedor'].values[0]
-        vendas = tabelaVendas.loc[tabelaVendas['Vendas'] > 50000, 'Vendas'].values[0]
+        vendedor = tabelaVendas.loc[tabelaVendas['Vendas'] > 55000, 'Vendedor'].values[0]
+        vendas = tabelaVendas.loc[tabelaVendas['Vendas'] > 55000, 'Vendas'].values[0]
         print(f'No mes {mes} alguém bateu a meta. Vendedor: {vendedor}, Vendas: {vendas}  \n')
         message = client.messages.create(
             body=f"No mes {mes} alguém bateu a meta.\nVendedor: {vendedor} \nVendas: {vendas}  \n",
-            from_="+15746525935",
+            from_="Twillio Number",
             to="Your Number"
         )
     else:
